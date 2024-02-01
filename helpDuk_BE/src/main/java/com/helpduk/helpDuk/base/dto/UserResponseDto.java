5bd5a134190422d1e3c023cf67341da02d9bf1b0
soyoon1 <20210841@duksung.ac.kr>
2024-02-02 03:22:31 +0900

    Feat: 다른 사용자 페이지 보여주기 기능 추가

---
+++ helpDuk_BE/src/main/java/com/helpduk/helpDuk/base/dto/UserResponseDto.java
@@ -0,0 +1,23 @@
+package com.helpduk.helpDuk.base.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserResponseDto {
+    // 타회원 페이지에 반환될 내용
+    private String nickName;
+    private String profileImage;
+    private Float temperature;
+
+    private Integer reviewCnt;
+
+    private List<ReviewDto> reviewDtoList;
+
+}
--- helpDuk_BE/src/main/java/com/helpduk/helpDuk/service/UserService.java
+++ helpDuk_BE/src/main/java/com/helpduk/helpDuk/service/UserService.java
@@ -1,9 +1,33 @@
 package com.helpduk.helpDuk.service;
 
+import com.helpduk.helpDuk.base.dto.ReviewDetailDto;
+import com.helpduk.helpDuk.base.dto.UserResponseDto;
+import com.helpduk.helpDuk.entity.UserEntity;
+import com.helpduk.helpDuk.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final ReviewService reviewService;
+
+    @Transactional
+    public UserResponseDto getOtherUserInformation(Integer userId){
+
+        UserEntity otherUser = userRepository.findByUserId(userId).orElseThrow();
+        ReviewDetailDto reviewDetailDto = reviewService.getReviewDetail(userId);
+
+        return UserResponseDto.builder()
+                .nickName(otherUser.getNickName())
+                .profileImage(otherUser.getProfileImage())
+                .temperature(otherUser.getTemperature())
+                .reviewCnt(reviewDetailDto.getReviewCnt())
+                .reviewDtoList(reviewDetailDto.getReviewDtoList())
+                .build();
+    }
+
 }
