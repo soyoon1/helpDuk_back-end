9f6d1def54f964d68f64db36f7c550198bad95f8
soyoon1 <20210841@duksung.ac.kr>
2024-02-01 10:14:15 +0900

    Comment: ChatRoomEntity 이전 버전 주석 삭제

--- helpDuk_BE/src/main/java/com/helpduk/helpDuk/entity/ChatRoomEntity.java
+++ helpDuk_BE/src/main/java/com/helpduk/helpDuk/entity/ChatRoomEntity.java
@@ -1,37 +1,3 @@
-//package com.helpduk.helpDuk.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.scheduling.config.Task;
-//
-//@Data
-//@Entity
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name="chatroom")
-//public class ChatRoomEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer roomId;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "taskId")
-//    private TaskEntity taskId;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "userId")
-//    private UserEntity userId;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "helpUserId")
-//    private UserEntity helperUserId;
-//
-//}
-
 package com.helpduk.helpDuk.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
