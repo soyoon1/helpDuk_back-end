4f9631d17da4cbb95ddc2a7fb6e267902722088d
yuchaemin2 <kusuri94@gmail.com>
2024-02-02 22:32:42 +0900

    Feat: React 연동 기초세우기

---
+++ helpDuk_BE/frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    fetch("hello")
+        .then((response) => {
+          return response.json();
+        })
+        .then(function (data) {
+          setMessage(data);
+        });
+  }, []);
+
+  return (
+      <div>
+        <div>시작</div>
+        <ul>
+          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+        </ul>
+      </div>
+  );
+}
+
+export default App;
